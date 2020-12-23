5f6814728cdf8bdf6a28932d4c5e0fd1ef6ad230
zadrogg <koreecrulit@gmail.com>
2020-12-24 02:43:31 +0300

    CategoryController-update

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -19,7 +19,7 @@ class CategoryController extends BaseController
         //dd($paginator);
         //dd($dsd, $paginator);
 
-        return view('blog.admin.category.index', compact('paginator'));
+        return view('blog.admin.categories.index', compact('paginator'));
     }
 
     /**
@@ -58,7 +58,7 @@ class CategoryController extends BaseController
         //dd(collect($item)->pluck('id'));
         $categoryList = BlogCategory::all();
 
-        return view('blog.admin.category.edit', compact('item', 'categoryList'));
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
     /**
@@ -70,6 +70,25 @@ class CategoryController extends BaseController
      */
     public function update(Request $request, $id)
     {
-        dd(__METHOD__, $request->all(), $id);
+        $item = BlogCategory::find($id);
+    
+        if (empty($item)) {
+            return back()
+                ->withErrors(['msg' => "Запись id=[$id] не найдена"])
+                ->withInput();
+        }
+
+        $data = $request->all();
+        $result = $item->fill($data)->save();
+
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.categories.edit', $item->id)
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Ошибка сохранения'])
+                ->withInput();
+        }
     }
 }
