00b8d6e974d0cb6c73fec713233400f37e3d12b0
zadrogg <koreecrulit@gmail.com>
2021-01-08 21:10:18 +0300

    queue/jobs

--- app/Http/Controllers/Blog/Admin/blogPostController.php
+++ app/Http/Controllers/Blog/Admin/blogPostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Blog\Admin;
 use App\Http\Controllers\Blog\Admin\BaseController;
 use App\Http\Requests\BlogPostCreateRequest;
 use App\Http\Requests\BlogPostUpdateRequest;
+use App\Jobs\BlogPostAfterCreateJob;
+use App\Jobs\BlogPostAfterDeleteJob;
 use App\Models\BlogPost;
 use App\Repositories\BlogCategoryRepository;
 use App\Repositories\BlogPostRepository;
@@ -75,6 +77,12 @@ class blogPostController extends BaseController
         $item = (new BlogPost())->create($data);
 
         if ($item) {
+            $job = new BlogPostAfterCreateJob($item);
+            // dd($job);
+            $this->dispatch($job);
+            // $this->dispatch(new BlogPostAfterCreateJob($item));
+
+
             return redirect()->route('blog.admin.posts.edit', [$item->id])
                 ->with(['success' => 'Успешно сохранено']);
         } else {
@@ -158,6 +166,27 @@ class blogPostController extends BaseController
         // $result = BlogPost::find($id)->forceDelete();
 
         if ($result) {
+
+            BlogPostAfterDeleteJob::dispatch($id)->delay(20);
+
+            //> Варианты запуска
+
+            // BlogPostAfterDeleteJob::dispatchNow($id);
+
+            // dispatch(new BlogPostAfterDeleteJob($id));
+            // dispatch_now(new BlogPostAfterDeleteJob($id));
+
+            // $this->dispatch(new BlogPostAfterDeleteJob($id));
+            // $this->dispatchNow(new BlogPostAfterDeleteJob($id));
+
+
+
+
+            /* $job = BlogPostAfterDeleteJob($id);
+            $job->handle();*/
+
+            //< Варианты запуска
+
             return redirect()
                 ->route('blog.admin.posts.index')
                 ->with(['success' => "Запись id[$id] удалена"]);
---
+++ app/Jobs/BlogPostAfterDeleteJob.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class BlogPostAfterDeleteJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var int
+     */
+    private $blogpostId;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param int $blogpostId
+     * 
+     * @return void
+     */
+    public function __construct($blogpostId)
+    {
+        $this->blogpostId = $blogpostId;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        logs()->warning("Удалена запись в блоге [{$this->blogpostId}]");
+    }
+}
